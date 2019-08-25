0dc13271a92b2de3c58ad3b7f339d0bc2c111770
pamelars86 <pamela.rosales@pucp.pe>
2019-08-25 11:01:50 -0300

    unnecessary files were removed

--- main-file/construction-assets/js/script.js
+++
@@ -1,6 +0,0 @@
-var swiper = new Swiper('.main_slider_construction', {
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-});
--- main-file/gym-fitness-assets/js/script.js
+++
@@ -1,7 +0,0 @@
-
-var swiper = new Swiper('.gym_main_slider', {
-    effect: 'fade',
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-});
-
