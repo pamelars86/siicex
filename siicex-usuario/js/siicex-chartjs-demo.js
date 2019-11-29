aa401305a179e07bceb0ff1d144853f08da213ab
pamelars86 <pamela.rosales@pucp.pe>
2019-11-29 13:44:21 -0300

    noticias destacadas - html

---
+++ siicex-usuario/js/siicex-chartjs-demo.js
@@ -0,0 +1,60 @@
+$(function () {
+
+
+    var ctx = document.getElementById('barChart');
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['2004', '2006', '2010', '2012', '2018'],
+        xAxisID: 'Millones',
+        yAxisID: 'Años',
+        datasets: [{
+            label: 'FOB',
+            data: [8000, 5000, 8500, 2500, 9300 ],
+            backgroundColor: [
+                '#ff9d00',
+                '#ff9d00',
+                '#ff9d00',
+                '#ff9d00',
+                '#ff9d00'
+            ],
+            borderColor: [
+                '#ff9d00',
+                '#ff9d00',
+                '#ff9d00',
+                '#ff9d00',
+                '#ff9d00'
+            ],
+            borderWidth: 1
+        },{
+            label: 'Volumen en Kg.',
+            data: [6700, 7500, 4100, 2500, 9100],
+            backgroundColor: [
+                '#7eba41',
+                '#7eba41',
+                '#7eba41',
+                '#7eba41',
+                '#7eba41'
+            ],
+            borderColor: [
+                '#7eba41',
+                '#7eba41',
+                '#7eba41',
+                '#7eba41',
+                '#7eba41'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+});
