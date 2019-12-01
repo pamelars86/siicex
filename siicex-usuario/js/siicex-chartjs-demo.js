13855b9ec59995f2c3a7a8adf179b935f164b3f2
pamelars86 <pamela.rosales@pucp.pe>
2019-12-01 12:39:01 -0300

    graficos linea. buscador modal. detalle noticia

--- siicex-usuario/js/siicex-chartjs-demo.js
+++ siicex-usuario/js/siicex-chartjs-demo.js
@@ -1,7 +1,7 @@
 $(function () {
 
-
-    var ctx = document.getElementById('barChart');
+    /* Gráfico de barras */
+  /*  var ctx = document.getElementById('barChart'); 
     var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -55,6 +55,64 @@ $(function () {
             }]
         }
     }
+});*/
+
+var lineData = {
+    labels: ["SEP", "OCT", "NOV", "DIC", "ENE", "FEB", "MAR", "ABR", "MAY"],
+    datasets: [
+        {
+            label: "2017",           
+            lineTension: 0,
+            borderColor: '#d81e05',          
+            fillColor: "#fff",
+            strokeColor: "#fff",
+            pointColor: "#fff",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            backgroundColor: 'transparent',
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40, 31, 60]
+        },
+        {
+            label: "2018",        
+            lineTension: 0,
+            borderColor: '#7eba41',
+            fillColor: "#7eba41",
+            strokeColor: "#7eba41",
+            pointColor: "rgba(26,179,148,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            backgroundColor: 'transparent',
+            pointHighlightStroke: "rgba(26,179,148,1)",
+            data: [28, 48, 40, 19, 86, 27, 90, 56, 70]
+        }
+    ]
+};
+
+var lineOptions = {
+    scaleShowGridLines: true,
+    lineTension: 0,           
+    scaleGridLineColor: "#7eba41",
+    scaleGridLineWidth: 1,
+    bezierCurve: true,
+    bezierCurveTension: 0.4,
+    pointDot: true,
+    pointDotRadius: 4,
+    pointDotStrokeWidth: 1,
+    pointHitDetectionRadius: 20,
+    datasetStroke: true,
+    datasetStrokeWidth: 2,
+    datasetFill: true,
+    responsive: true,
+};
+
+
+/* Gráfico de líneas*/
+var ctxLine = document.getElementById('lineChart'); 
+var myLineChart = new Chart(ctxLine, {
+    type: 'line',
+    data: lineData,
+    options: lineOptions
 });
 
 });
